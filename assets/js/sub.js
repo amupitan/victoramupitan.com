25779fcd84d8eb9da80f40b2496c52b2163e76dd
Victor Amupitan <amupitan@iastate.edu>
2018-03-12 17:49:28 -0500

    adjust work logo size and enhance content changing

--- assets/js/sub.js
+++ assets/js/sub.js
@@ -1,3 +1,17 @@
+const workContent = {
+    workiva: [
+        'I developed features on a spreadsheets web application using Dart Language to improve functionality',
+        'I integrated application with other critical internal services using Go Language',
+        'I implemented major user-experience improvements, coordinating closely with UX resources',
+    ],
+    'isu-gdcb': [
+        'I developed web interfaces using technologies like Django, for command line tools developed by the GDC Biology Department',
+    ],
+    'isu-dor': [
+        'I assist students in adjusting to campus living',
+    ],
+};
+
 (function ($) {
 
     const currentSelected = 'current';
@@ -13,8 +27,14 @@
     const replaceTabContent = (tabName) => {
         // document.getElementById('work-image').setAttribute('src', `images/${tabName}.jpg`);
         const textDiv = document.getElementById('work-text');
-        textDiv.children[0].textContent = 'Hello';
-        textDiv.children[1].textContent = 'Hello2';
+        textDiv.innerHTML = '';
+        const ul = document.createElement('ul');
+        for (const line of workContent[tabName]) {
+            const li = document.createElement('li');
+            li.innerHTML = line;
+            ul.appendChild(li);
+        }
+        textDiv.appendChild(ul);
     };
 
     const deactiveTabs = () => {
@@ -30,7 +50,7 @@
             evt.target.classList.add(currentSelected);
 
             // TODO replace content
-            replaceTabContent('');
+            replaceTabContent(tab.dataset.name);
             console.log('happened3');
         };
     };
