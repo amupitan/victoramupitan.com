7d5b3958f078027640723b5ff256a647dbc6f245
Victor Amupitan <amupitan@iastate.edu>
2018-04-14 01:08:18 -0500

    complete work phase

--- assets/js/sub.js
+++ assets/js/sub.js
@@ -1,15 +1,30 @@
 const workContent = {
-    workiva: [
-        'I developed features on a spreadsheets web application using Dart Language to improve functionality',
-        'I integrated application with other critical internal services using Go Language',
-        'I implemented major user-experience improvements, coordinating closely with UX resources',
-    ],
-    'isu-gdcb': [
-        'I developed web interfaces using technologies like Django, for command line tools developed by the GDC Biology Department',
-    ],
-    'isu-dor': [
-        'I assist students in adjusting to campus living',
-    ],
+    workiva: {
+        name: 'Workiva Inc.',
+        position: 'Software Engineering Intern',
+        date: 'May 2017 - August 2017',
+        tasks: [
+            'Developed features on a spreadsheets web application using Dart Language',
+            'Integrated application with other critical internal services using Go Language',
+            'Implemented major user-experience improvements, coordinating closely with UX resources',
+        ],
+    },
+    'isu-gdcb': {
+        name: 'Iowa State University - Genetics, Development & Cell Biology Department',
+        position: 'Full-Stack Web Developer & Research Assistant',
+        date: 'August 2016 – May 2017',
+        tasks: [
+            'Developed web interfaces using technologies like Django, for command line tools developed by the GDC Biology Department',
+        ],
+    },
+    'isu-dor': {
+        name: 'Iowa State University - Department of Residence',
+        position: 'Community Advisor (Resident Assistant)',
+        date: 'August 2017 – May 2018',
+        tasks: [
+            'Assisted students in adjusting to campus living',
+        ],
+    },
 };
 
 (function ($) {
@@ -25,11 +40,14 @@ const workContent = {
     };
 
     const replaceTabContent = (tabName) => {
-        // document.getElementById('work-image').setAttribute('src', `images/${tabName}.jpg`);
+        document.getElementById('work-image').setAttribute('src', `images/${tabName}.jpg`);
         const textDiv = document.getElementById('work-text');
-        textDiv.innerHTML = '';
+        textDiv.innerHTML = `
+        <strong><p class='no-margin align-left'>${workContent[tabName].name}</p></strong>
+        <p class='no-margin align-left'>${workContent[tabName].position}</p>
+        <p class='align-left'>${workContent[tabName].date}</p>`;
         const ul = document.createElement('ul');
-        for (const line of workContent[tabName]) {
+        for (const line of workContent[tabName].tasks) {
             const li = document.createElement('li');
             li.innerHTML = line;
             ul.appendChild(li);
@@ -49,7 +67,6 @@ const workContent = {
             deactiveTabs();
             evt.target.classList.add(currentSelected);
 
-            // TODO replace content
             replaceTabContent(tab.dataset.name);
             console.log('happened3');
         };
